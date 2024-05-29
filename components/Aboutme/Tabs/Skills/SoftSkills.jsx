0a25a49f7b4212d98f52ddd118b3cc38d31b7e37
JeyBySy <njcocosa@gmail.com>
2024-05-30 04:59:06 +0800

    General maintenance and updates

--- components/Aboutme/Tabs/Skills/SoftSkills.jsx
+++ components/Aboutme/Tabs/Skills/SoftSkills.jsx
@@ -4,7 +4,7 @@ const SoftSkills = () => {
     return (
         <div className='flex flex-wrap gap-5 text-muted'>
             <li className='list-disc'>
-                I do <span className='text-green-300 font-extrabold'>communicate</span> with others in a way that&apos;s easy to understand and respond to. I make sure to listen and show that I value their ideas, jokes, or concerns. This helps us work together better and solve problems as a team.
+                I know how to <span className='text-green-300 font-extrabold'>communicate</span> with others in a way that&apos;s easy to understand and respond to. I make sure to listen and show that I value their ideas, jokes, or concerns. This helps us work together better and solve problems as a team.
             </li>
             <li className='list-disc'>
                 As a software developer, I&apos;ve consistently demonstrated strong <span className='text-green-300 font-extrabold'>teamwork</span> skills throughout my career. I actively engage with team members, share insights, and collaborate effectively to achieve project goals. My ability to communicate technical concepts clearly and work cohesively within a team environment has been instrumental in delivering successful projects.
--- components/Global/Footer/page.js
+++ components/Global/Footer/page.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='flex justify-center p-4 m-8'>
-            <p className='cursor-default text-white rounded-full p-2 border  sm:text-[12px] md:text-base lg:text-lg xl:text-sm'>
+        <footer className='flex justify-center p-4 bg-gray-900'>
+            <p className='cursor-default text-white rounded-full p-2 border sm:text-[12px] md:text-base lg:text-lg xl:text-sm'>
                 Made with ❤️ by <span className='text-green-300 font-bold'>Niño Jherico Cocosa</span> using <a className='text-blue-500 hover:underline' href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a>
             </p>
         </footer>
