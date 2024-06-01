1abf86312062eaa22da4d5d2d8fe7f220eb0cfab
JeyBySy <njcocosa@gmail.com>
2024-06-02 04:48:59 +0800

    General maintenance and updates

--- components/Global/Modal/page.jsx
+++ components/Global/Modal/page.jsx
@@ -2,7 +2,7 @@ const Modal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed w-[95%] m-auto inset-0 rounded flex items-center justify-center bg-[#3d3d3d]">
+        <div className="fixed w-[95%] m-auto inset-0 rounded flex items-center justify-center bg-[#3d3d3df5]">
             <div className="bg-white p-4 relative">
                 <button className="z-10 absolute top-0 right-5" > <span className="text-[3em] cursor-pointer" onClick={onClose}>&times;</span></button>
                 {children}
