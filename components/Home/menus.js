93f0e74e12519c28deac706fe19c3dd8c9c5d9c5
JeyBySy <njcocosa@gmail.com>
2024-11-23 21:16:51 +0800

    Change link in resume

--- components/Home/menus.js
+++ components/Home/menus.js
@@ -25,7 +25,7 @@ export const menus = [
     },
     {
         name: 'Resume',
-        link: 'https://drive.google.com/file/d/16wp4SeeMtGhr4dzNCk_zM9tmoRUxymCP/view?usp=sharing',
+        link: 'https://drive.google.com/file/d/1IXBMw02JsAgjDEPiz698gzc6Ia2H0ztk/view?usp=sharing',
         icon: "/img/file.svg",
         isLink: true
     },
