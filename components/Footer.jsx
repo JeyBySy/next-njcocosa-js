ada13d8346e261d50fe197b962e30b3354455dd9
JeyBySy <njcocosa@gmail.com>
2025-05-18 00:23:43 +0800

    Update year

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="relative m-auto px-4 sm:px-6 lg:px-8 bg-mine-950 p-7 w-full text-center">
             <span className='text-sm'>
-                Made with ❤️ by Niño Jherico Cocosa using Next.js - 2024
+                Made with ❤️ by Niño Jherico Cocosa using Next.js - 2025
             </span>
         </div>
     )
